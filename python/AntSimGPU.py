8d0920a6c00630e5d86dddc6660f5cce7408a7c2
nighthawkbravo <lucas@aggati.com>
2021-11-12 20:32:22 +0100

    Testing numba with flask

--- python/AntSimGPU.py
+++ python/AntSimGPU.py
@@ -3,11 +3,11 @@
 
 from __future__ import division
 from numba import cuda
-#from flask import Flask
+from flask import Flask
 import numpy
 import math
 
-
+app = Flask(__name__)
 
 # CUDA kernel
 @cuda.jit
@@ -16,13 +16,14 @@ def my_kernel(io_array):
     if pos < io_array.size:
         io_array[pos] *= 2 # do the computation
 
-# Host code   
+# Host code
+"""
 data = numpy.ones(256)
 threadsperblock = 256
 blockspergrid = math.ceil(data.shape[0] / threadsperblock)
 my_kernel[blockspergrid, threadsperblock](data)
 print(data)
-
+"""
 
 
 
@@ -31,11 +32,17 @@ print(data)
 
 #app = Flask(__name__)
 
-#@app.route('/')
-#def hello_world():
-#   return 'Hello World'
+@app.route('/')
+def hello_world():
+    data = numpy.ones(256)
+    threadsperblock = 256
+    blockspergrid = math.ceil(data.shape[0] / threadsperblock)
+    my_kernel[blockspergrid, threadsperblock](data)
+    out_arr = numpy.array_str(data)    
+    return out_arr
+   #return 'Hello World'
 
-#if __name__ == '__main__':
-#   app.run() 
+if __name__ == '__main__':
+   app.run() 
    # app.run(host='127.0.0.1',port=8000,debug=True)
 
